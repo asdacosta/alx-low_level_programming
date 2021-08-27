c26554304b8507410e05fc75149f76cb8930099f
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2021-08-26 22:26:56 -0700

    Updated 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - Prints "Programming is like a multilingual puzzle"
+ *	 followed by a new line.
  *
  * Return: Always 0 (Success)
  */
@@ -9,3 +10,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
