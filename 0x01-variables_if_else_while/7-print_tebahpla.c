7e3d699d32bd68b3bfc972d40d694f9e8bad5408
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2021-08-28 09:39:57 -0700

    Added 7-print_tebahpla.c file

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,14 +4,19 @@
  * main - Prints reversed alphabet.
  *
  * Return: Always 0 (Success)
+ * Alternate method :
+ * for (alp = 122; alp > 96; alp--)
+ * {
+ * putchar(alp);
+ * }
  */
 int main(void)
 {
-	int i;
+	int alp;
 
-	for (i = 122; i > 96; i--)
+	for (alp = 'z'; alp >= 'a'; alp--)
 	{
-		putchar(i);
+		putchar(alp);
 	}
 	putchar('\n');
 	return (0);
