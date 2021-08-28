ee8a91611a5c91711963f033112eb702e674ad59
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2021-08-28 09:16:20 -0700

    Updated 6-print_numberz.c file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,10 +4,11 @@
  * main - Prints numbers between 0 to 9.
  *
  * Return: Always 0 (Success)
- * Alternate method - for (i = 48; i < 58; i++)
- * 			{
- * 				putchar(i);
- * 			}
+ * Alternate method :
+ *  for (i = 48; i < 58; i++)
+ * {
+ * 	putchar(i);
+ * }
  */
 int main(void)
 {
