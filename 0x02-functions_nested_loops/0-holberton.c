b6517555051fbbd5f63d12d012eef6bc9ea8b20c
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2021-08-31 08:37:05 -0700

    Updated 0-holberton.c file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,14 +7,12 @@
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+	int i = 0;
+	char s[] = "_putchar\n";
+	while (s[i])
 	{
-		_putchar(str[count]);
+		_putchar (s[i]);
+		i++;
 	}
-	_putchar('\n');
 	return (0);
 }
