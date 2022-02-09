4f6d4634ba7550c3b0e2ae33a38ad202ee83025b
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-09 18:11:32 +0000

    Updated: adjusting to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Prints "Programming is like a multilingual puzzle"
- *       followed by a new line.
+ *      followed by a new line.
  * 
  * Return: Always 0 (Success)
  */
