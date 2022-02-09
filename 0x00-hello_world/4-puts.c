0f919bc0f5363f10357cc711c8c5fe5b24e4f1db
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-09 18:38:00 +0000

    Updated: adjusting to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Prints "Programming is like a multilingual puzzle"
- *      followed by a new line.
- * 
+ * 	followed by a new line.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
