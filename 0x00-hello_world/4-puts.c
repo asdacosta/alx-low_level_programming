f584561ba71cabb29265251dab5c947d80e9cccd
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-09 17:48:37 +0000

    Created: prints string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Prints "Programming is like a multilingual puzzle"
- *	 followed by a new line.
- *
- * Return: Always 0 (Success)
+ * main - Prints "Programming is like..."
+ * 
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+    puts("Programming is like building a multilingual puzzle \n");
 
+    return (0);
+}
