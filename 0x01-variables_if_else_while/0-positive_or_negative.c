87ec1daef52411943f660dbbe32fbed42e33be25
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-10 12:21:05 +0000

    Created: random number positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - Assigns random number to variable each time it is executed and prints
+ * 	whether it is negative or positive.
+ * @n: Random number 
  *
  * Return: Always 0 (Success)
  */
@@ -13,18 +15,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
+
+	if (n>0)
 		printf("%d is positive\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else
-	{
 		printf("%d is negative\n", n);
-	}
+
 	return (0);
 }
 
