5ac9ca0e8d1b089bd130163eed1d887eefe9533f
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-10 13:02:01 +0000

    Updated: corrected if statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,11 +16,11 @@ int main(void)
 			printf("and is greater than 5");
 		else if (last_digit == 0)
 			printf("and is 0");
-		else if (last_digit < 6 && != 0)
+		else if ((last_digit < 6) && (last_digit != 0))
 			printf("and is less than 6 and not 0");
 	}
 
-	printf("Last digit of %d is %d %s", n, last_digit, string());
+	printf("Last digit of %d is %d %c", n, last_digit, string());
 
 	return (0);
 }
