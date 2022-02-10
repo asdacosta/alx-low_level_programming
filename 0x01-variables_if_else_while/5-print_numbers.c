614c5a0d2251c556df9bba0d4d8f8c236fed48b6
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-10 16:36:01 +0000

    Created: prints all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 from 0, followed by new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+		putchar(m);
+	putchar('\n');
+
+	return (0);
+}
+
