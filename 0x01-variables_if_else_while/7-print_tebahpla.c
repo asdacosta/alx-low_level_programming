46e1dec659fe338f80d3080fbf48ee5683af7b48
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-10 17:09:57 +0000

    Creation: print lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabet in reverse.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 'z'; m <= 'a'; m--)
+		putchar(m);
+	putchar('\n');
+
+	return (0);
+}
+
