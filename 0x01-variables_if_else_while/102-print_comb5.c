55f129daafe386ae3c7858ab65ff7e4e85e17db0
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-11 09:29:07 +0000

    Creation: prints combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combinations of two two-digit numbers from 0 to 99.
+ *	The two numbers are separated by space and combination of
+ *	numbers are separated by comma and followed by space.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+				putchar(n);
+				putchar(m);
+
+				if (n != '10')
+					putchar(' ');
+				
+				if ((n < m) || (n = m) || (n > m))
+				{
+					putchar(n);
+					putchar(m);
+				}
+
+				if (m != '10')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
