b704e7dad7e5a663bc4e819ca5d550ef4fb1c111
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-11 19:59:31 +0000

    Creation: checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _isalpha - Checks for alphabetic character
+ * @c: Character to check
+ *
+ * Return: 1 if c is a letter and 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	int n;
+
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else 
+		return (0);
+}
+
