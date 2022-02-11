4415dda767fc3f911df5ff587fe7d907e4fc1270
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-11 02:30:25 +0000

    Creation: print different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Prints different combination of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+	int m;
+
+	while ((n >= '0') && (n <= '8'))
+	{
+		putchar(n);
+
+		for (m = '1'; m <= '9'; m++)
+		{
+			putchar(m);
+			
+			if (m != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
