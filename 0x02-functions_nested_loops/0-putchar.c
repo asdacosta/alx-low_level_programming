f1c8cfc04a1d174a4fdd6beaf1c9b5adf8e6aede
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-11 16:39:16 +0000

    Creation: print _putchar without using stlib

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar followed by a new line.
+ * @c: characters to be printed
+ *
+ * Return: Always 9 (Success)
+ */
+int main(char c)
+{
+	c = "_putchar";
+
+	_putchar(c);
+	_putchar('\n');
+
+	return (0);
+}
+
