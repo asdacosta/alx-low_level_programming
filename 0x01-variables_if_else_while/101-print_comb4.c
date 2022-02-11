3fae0c2d912b97a09b41947e4cbe50f06a04854a
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-11 08:51:11 +0000

    Creation: prints smallest combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints smallest combinations of three digits. Each number is separated by
+ *	comma ',' and space.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, m, p;
+
+	for (n = '0'; n < '8'; n++)
+	{
+		for (m = '1'; m < '9'; m++)
+		{
+			for (p = '2'; p <= '9'; p++)
+			{
+				if (n < m < p)
+				{
+					putchar(m);
+					putchar(n);
+					putchar(p);
+
+					if (n != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
