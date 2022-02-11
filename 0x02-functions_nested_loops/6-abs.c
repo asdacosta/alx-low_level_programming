0106f62ef4bbae7705e296bab9f2f6bee3b8cb2e
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-11 20:56:50 +0000

    Updated: inserted variable in argument

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,12 @@
 
 /**
  * _abs - Computes the absolute value of an integer
+ * @n: Number to check the absolute value
  *
  * Return: Always 0 (Success)
  */
-int _abs(int)
+int _abs(int n)
 {
-	int n;
-
 	if (n >= 0)
 		_putchar(n);
 	else 
