c0fc3f563ef58aebda650a6e1f15881fd042dca4
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-12 18:46:17 +0000

    Deleted: Removed relational statements in for loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n < 0)
 	{
-		for (n < 0; n <= 98; n++)
+		for (; n <= 98; n++)
 		{
 			_putchar(n);
 			_putchar(',');
@@ -31,7 +31,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n > 0; n <= 98; n++)
+		for (; n <= 98; n++)
 		{
 			_putchar(n);
 			_putchar(',');
