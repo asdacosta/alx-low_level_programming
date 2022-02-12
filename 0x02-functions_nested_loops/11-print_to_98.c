d4dadead0259f510bd42e2a3c31e86047b4b843c
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-12 18:41:13 +0000

    Creation: Print natural numbers from number to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98 followed
+ *	by a new line.
+ * @n: Number to start from.
+ *
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+	if (n < 0)
+	{
+		for (n < 0; n <= 98; n++)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	else if (n == 0)
+	{
+		for (n = 0; n <= 98; n++)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (n > 0; n <= 98; n++)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
