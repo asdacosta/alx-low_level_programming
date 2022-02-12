79b965ced4b78b15d854a25b4cc042855118592d
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-12 01:12:05 +0000

    Creation: print last digit of number

---
+++ 0x02-functions_nested_loops/7-print_lasst_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit of number
+ * @n: Number to obtain last digit
+ *
+ * Return: Last digit of number
+ */
+int print_last_digit(int n)
+{
+	if ((n >= -9) && (n <=9)
+			return (n);
+	else if ((n < -9) && (n > 9))
+			return (n[-1]);
+}
+
