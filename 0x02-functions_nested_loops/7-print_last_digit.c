1ac3e82d44b4d5f17a0bc9fe6cde334141ef758c
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-12 02:44:45 +0000

    Updated: Inserted variables and _putchar to print all last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,11 @@ int print_last_digit(int n)
 {
 	int m = n % 10;
 
-	if ((n >= -9) && (n <=9))
-		return (n);
-	else
+	if (m >= 0)
+		_putchar(m + '0');
 		return (m);
+	else
+		_putchar(-m + '0');
+		return (-m);
 }
 
