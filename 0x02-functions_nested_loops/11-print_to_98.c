920aae7a5900d1524ed700ddd7eb11e6bace3ef3
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-12 19:54:22 +0000

    Updated: Making sure numbers greater than 98 is printed and there is no comma at end

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,27 +9,22 @@
  */
 void print_to_98(int n)
 {
-	if (n < 0)
+	if (n <= 98)
 	{
 		for (; n <= 98; n++)
 		{
 			printf("%d, ", n);
+			if (n == 98)
+				printf("%d\n", n);
 		}
-		printf("\n");
 	}
-	else if (n == 0)
+	else if (n > 98)
 	{
-		for (; n <= 98; n++)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
-		}
-		printf("\n");
-	}
-	else if (n > 0)
-	{
-		for (; n <= 98; n++)
-		{
-				printf("%d, ", n);
+			if (n == 98)
+				printf("%d\n", n);
 		}
 		printf("\n");
 	}
