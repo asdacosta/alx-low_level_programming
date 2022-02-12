e990622cb551808e6cf9ae3435b49aad35a61108
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-12 20:29:14 +0000

    Creation: Prints times table starting with 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - Prints n times table starting with o
+ * @n: Number to print 
+ *
+ * Return: Void
+ */
+void print_times_table(int n)
+{
+	if ((n > 15) || (n < 0))
+	{
+		printf("\n");
+	}
+	else if ((n <= 15) && (n >= 0))
+	{
+		for (; n >= 0; n++)
+		{
+			printf("%d, ", n);
+			printf("\n");
+		}
+	}
+}
+
