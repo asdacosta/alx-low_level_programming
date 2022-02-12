4747bfc1261e8960ed8bcf607973e058ba9d92f3
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-12 13:42:19 +0000

    Creation: Adds two integers and returns the value

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @m: First operand
+ * @n: Second operand
+ *
+ * Return: Result of the operands
+ */
+int add(int m, int n)
+{
+	int result;
+
+	result = m + n;
+	return (result);
+}
+
