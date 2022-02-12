e2f0d2d21e97cd2d545e6794fcebac8528d315e0
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-12 19:58:34 +0000

    Updated: Making sure last number printed has no comma

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,11 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (; n <= 98; n++)
 		{
 			printf("%d, ", n);
-			if (n == 98)
-				printf("%d\n", n);
 		}
 	}
 	else if (n > 98)
@@ -23,10 +21,8 @@ void print_to_98(int n)
 		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
-			if (n == 98)
-				printf("%d\n", n);
 		}
-		printf("\n");
 	}
+	printf("%d\n", n);
 }
 
