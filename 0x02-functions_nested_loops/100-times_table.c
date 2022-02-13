885f3ef752c12eb78d3583313ebaad3162f7b8c2
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-13 00:30:17 +0000

    Updated: Changed header file and used new approach

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_times_table - Prints n times table starting with o
@@ -8,24 +8,38 @@
  */
 void print_times_table(int n)
 {
-	if ((n > 15) || (n < 0))
+	int a, b, op;
+
+	if (n >= 0 && n <= 15)
 	{
-		printf("\n");
-	}
-	else if (n <= 15)
-	{
-		for (; n <= 15; n--)
+		for (a = 0; a <= n; a++)
 		{
-			printf("%d, ", n);
-			printf("\n");
-		}
-	}
-	else if (n >= 0)
-	{
-		for (; n >= 0; n++)
-		{
-			printf("%d, ", n);
-			printf("\n");
+			_putchar(48);
+			for (b = 1; b <= n; b++)
+			{
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (op <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putcahr(op + 48);
+				}
+				else if (op <= 99)
+				{
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+				else
+				{
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op /10) % 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+			}
+			_putchar('\n');
 		}
 	}
 }
