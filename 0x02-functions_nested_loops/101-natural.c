a98768c87227c717baf663321e37aa94620917ac
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-13 01:24:34 +0000

    Updated: Used one for loop nested with an if

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,34 +4,19 @@
  * main - Computes and prints the sum of all the multiples of 3 or 5 
  *	below 1024
  *
- * Return: Void
+ * Return: Always 0.
  */
 void main(void)
 {
-	int m, n, m3, m5, result3, result5, end, a, b;
-	m3 = 1024 / 3;
-	m5 = 1024 / 5;
+	int a, b;
 
-	if (end < 1024)
+	for (a = 1; a < 1024; a++)
 	{
-		for (m = 3; m < m3; m++)
-		{
-			for (n = 1; n < m3; n++)
-			{
-				result3 = m * n
-				for (a = 5; a < m5; a++)
-				{
-					for (b = 1; b < m5; b++)
-					{
-						result5 = a * b;
-						end = result3 + result5;
-						printf("%d ", end);
-					}
-				}
-			}
-		}
-
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b +=a;
 	}
-	printf("\n");
+	printf("%d\n", b);
+
+	return (0);
 }
 
