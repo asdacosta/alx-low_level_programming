306dd5580576a31e6081d6fb6b19cede4839b427
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-14 17:52:27 +0000

    Updated: Used if statements to filter to get Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,32 @@
  */
 int main(void)
 {
-	int n, m, p, q;
+	int n, m;
 
 	for (n = 1; n <= 100; n++)
 	{
-		for (m = 3; m <= 100; m + 3)
-		{
-			printf("Fizz ");
-		}
-		for (p = 5; p <= 100; p + 5)
-		{
-			printf("Buzz ");
-		}
-		for (q = 15; q <= 100; q + 15)
-		{
-			printf("FizzBuzz ");
-		}
-
-		printf("%d ", n);
+		for (m = 1; m <= 50; m++)
+			{
+				if (n == (3 * m))
+				{
+					printf("Fizz ");
+				}
+				else if (n == (5 * m))
+				{
+					printf("Buzz ");
+				}
+				else if (n == (15 * m))
+				{
+					printf("FizzBuzz ");
+				}
+				else
+				{
+					printf("%d ", n)
+				}
+			}
 	}
+	printf("\n");
+
 	return (0);
 }
 
