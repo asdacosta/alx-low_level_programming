758e861ce3ce5269d62007ec196dac0de079a6bd
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-14 12:51:35 +0000

    Updated: Handling multi-character error

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,14 @@ void more_numbers(void)
 {
 	int n, m;
 
-	m = '0';
-	while (m < '10')
+	m = 0;
+	while (m <= 9)
 	{
-		for (n = '0'; n <= '14'; n++)
+		for (n = 0; n <= 14; n++)
 		{
-			_putchar(n);
+			if (n >= 10)
+				_putchar((n / 10) + 48);
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 		m++;
