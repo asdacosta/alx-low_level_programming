a676860fe117019d385104834a8c04fb6c91a4aa
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-14 19:06:53 +0000

    Updated: Used a diff approach

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,29 +9,28 @@
  */
 int main(void)
 {
-	int n, m;
+	int n, m, m3, m5, m15;
+	m3 = 100 / 3;
+	m5 = 100 / 5;
+	m15 = 100 / 15;
 
 	for (n = 1; n <= 100; n++)
 	{
-		for (m = 1; m <= 100; m++)
-			{
-				if (n == (3 * m) && (n != 100))
-				{
-					printf("Fizz ");
-				}
-				else if (n == (5 * m) && (n != 100))
-				{
-					printf("Buzz ");
-				}
-				else if (n == (15 * m) && (n != 100))
-				{
-					printf("FizzBuzz ");
-				}
-				else
-				{
-					printf("%d ", n);
-				}
-			}
+		for (m = 1; m <= m3 ; m++)
+		{
+			if (n == (3 * m))
+				printf("Fizz ");
+		}
+		for (m = 1; m <= m5; m++)
+		{
+			if (n == (5 * m))
+				printf("Buzz ");
+		}
+		for (m = 1; m <= m15; m++)
+		{
+			if (n == (15 * n))
+				printf("FizzBuzz ");
+		}
 	}
 	printf("\n");
 
