3713f1794c9202b51ed6e841d174091a75c3b546
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-14 16:23:33 +0000

    Created: Draw a number of straight lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,28 @@
 #include "main.h"
 
 /**
- * print_line - prints straight line n times.
- * @n: times straight line is printed.
- * Return: no return.
+ * print_line - Draws a number of straight lines in the terminal
+ * @n: Number of straight lines
+ *
+ * Return: Void
  */
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
+	for (n = 0; n <= 10; n++)
 	{
-		_putchar('_');
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + '0');
+
+		if (n == 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
+
