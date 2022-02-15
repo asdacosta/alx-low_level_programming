752880468b9b448f3b74349fb5cbce888f57507d
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-15 00:15:13 +0000

    Updated: Added a for loop and extra to print as required

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,22 +8,30 @@
  */
 void print_number(int n)
 {
-	int m, d, count;
+	int p, d, count;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		p = n * -1;
 	}
 	else
 	{
-		m = n;
+		p = n;
 	}
 	
+	d = p;
+	count = 1;
+
 	while (d > 9)
 	{
 		d /= 10;
 		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((p / count) % 10) + 48);
 	}
 }
 
