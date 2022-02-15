3d12abfe11dd5e03eb0e1668e8f268b70f0c4f0f
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-15 20:07:42 +0000

    Created: Return length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,24 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string.
- * @s: input string.
- * Return: length of a string.
+ * _strlen - Returns the length of a string
+ * @s: Pointer to string
+ *
+ * Return: Always 0
  */
 int _strlen(char *s)
 {
-	int count = 0;
+	int n, m, len;
 
-	while (*(s + count) != '\0')
-		count++;
-	return (count);
+	for (n = 0; n <= 20; n++)
+	{
+		for (m = 0; m <= 20; m++)
+		{
+			*(s + n) = m + 1;
+			len = *(s + n);
+		}
+
+		_putchar((len % 10) + '0');
+	}
 }
+
