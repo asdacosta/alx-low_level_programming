ebdf371e7da0cd5f1fe79a5fc825ee8383a9591e
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2022-02-16 09:06:30 +0000

    Updated: Used paratmeter to check unused parameter error

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,19 +11,19 @@ void puts_half(char *str)
 	int n, m, p, q, a;
 
 	n = 0;
-	while (s[n] != '\0')
+	while (str[n] != '\0')
 	{
-		m = (s[n] + 1);
+		m = str[n] + 1;
 		n++;
 		p = m / 2;
 
 		for (q = 0; q <= p; q++)
 		{
 			_putchar(q);
-			if (q == (2q + 1))
+			if (q == (2 * q) + 1)
 			{
 				a = (m - 1) / 2;
-				_putchar(a)
+				_putchar(a);
 			}
 		}
 	}
