94e57be68c662b2fb8d2202191b0567126c1eed1
asdacosta <dacostasilvanus@gmail.com>
2022-02-19 18:13:56 +0000

    Updated: Cleared trailing white space

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,24 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int n = 0, m;
+	unsigned int i = 0;
 
-	while (*(s + n) != '\0')
-	{
-		if (*(s + n) == c)
-		{
-			m = 0;
-			while ((*(s + m) >= c) && (*(s + m) != '\0'))
-			{
-				return (s + m);
-			}
-		}
-		else
-		{
-			return ('\0');
-		}
-	}
-
-	return (s);
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
 
