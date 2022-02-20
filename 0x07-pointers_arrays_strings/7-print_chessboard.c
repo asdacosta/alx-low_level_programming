c67b09016ec5803a58326a50ad59b75b070317d5
asdacosta <dacostasilvanus@gmail.com>
2022-02-20 12:40:16 +0000

    Updated:Used sinlge quote to solve int issue

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,9 +10,9 @@ void print_chesssboard(char (*a)[8])
 {
 	int n;
 
-	for (n = 0; *(a + n) != '\0'; n++)
+	for (n = 0; n < *a; n++)
 	{
-		if (*(a + n) == 39 || *(a + n) == 44)
+		if (*(a + n) == '39' || *(a + n) == '44')
 		{
 			break;
 		}
