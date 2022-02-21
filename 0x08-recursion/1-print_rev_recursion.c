3b23e752c511ebf85720b399730e747f21c80708
asdacosta <dacostasilvanus@gmail.com>
2022-02-21 11:42:48 +0000

    Updated: Called function first to print string in rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\n')
-		break;
-	else
+	if (*s != '\0')
 	{
-		_print_rev_recursion(s - 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
