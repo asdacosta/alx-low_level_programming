4741b7f3368d291169f2fb6c32e800e242526140
asdacosta <dacostasilvanus@gmail.com>
2022-02-21 11:25:47 +0000

    Created: Prints string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse.
- * @s: string
- * Return: no return.
+ * _print_rev_recursion - Prints string in reverse
+ * @s: Pointer to string
+ *
+ * Return: Void
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\n')
+		break;
+	else
 	{
-		_print_rev_recursion(s + 1);
+		_print_rev_recursion(s - 1);
 		_putchar(*s);
 	}
 }
+
