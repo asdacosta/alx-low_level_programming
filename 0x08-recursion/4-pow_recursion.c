7cb8445e2f82d2551ac3f1644c1af04c711b6b51
asdacosta <dacostasilvanus@gmail.com>
2022-02-21 23:16:30 +0000

    Created: Returns the exponent of a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y.
- * @x: base.
- * @y: exponent.
- * Return: value of the exponentiation.
+ * _pow_recursion - Returns the value of x raised to the power y
+ * @x: Base number
+ * @y: Exponent number
+ *
+ * Return: Number raised to the exponent, and -1 if y is lower than 0
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (y == 0)
-		return (1);
 	else
-		return (x * _pow_recursion(x, y - 1));
+		return (x ** y);
 }
+
