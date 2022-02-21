86577f33fe2f96893a817bf544912e1b07a0406a
asdacosta <dacostasilvanus@gmail.com>
2022-02-21 22:15:00 +0000

    Udpated: Returns length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string
- * Return: the length of a string.
+ * _strlen_recursion - Prints the length of a string
+ * @s: Pointer to string
+ *
+ * Return: Always 0
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	int n;
+
+	if (*s != '\0')
+	{
+		*s = &n;
+		_strlen_recursion(s + 1);
+	}
+
+	return (0);
 }
+
