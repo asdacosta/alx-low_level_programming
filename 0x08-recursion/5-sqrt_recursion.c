993acef6ccc34b9e9e1912ef08177be8cbb71dc5
asdacosta <dacostasilvanus@gmail.com>
2022-02-22 00:20:21 +0000

    Created: Return nonnegative square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,24 @@
 #include "main.h"
 
 /**
- * power_operation - returns the natural square root of a number.
- * @n: input number.
- * @c: iterator.
- * Return: square root or -1.
- */
-int power_operation(int n, int c)
-{
-	if (c % (n / c) == 0)
-	{
-		if (c * (n / c) == n)
-			return (c);
-		else
-			return (-1);
-	}
-	return (0 + power_operation(n, c + 1));
-}
-/**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: input number.
- * Return: natural square root.
+ * _sqrt_recursion - Returns the natural or nonnegative square root
+ *	of a number.
+ * @n: The number
+ *
+ * Return: Natural square root, and -1 if n does not have a natrual
+ *	square root.
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (0);
+	int m;
+
 	if (n == 1)
 		return (1);
-	return (power_operation(n, 2));
+	else if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+	else if (n % m == 0)
+		return (_sqrt_recursion(n / m));
 }
+
