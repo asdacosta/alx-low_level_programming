25b49d2c54aa043baf3a484254f20e534bcff44e
asdacosta <dacostasilvanus@gmail.com>
2022-02-22 01:18:04 +0000

    Updated: Inserted another recursive function that checks for prime int

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * prime - Checks for prime number
+ * @n: Input number
+ * @c: Iterator
+ *
+ * Return: 1 if input int is a prime number, and 0 otherwise
+ */
+int prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + prime(n, c + 1));
+}
+
+/**
  * is_prime_number - Checks for a prime number
  * @n: Number to check
  *
@@ -8,11 +27,8 @@
  */
 int is_prime_number(int n)
 {
-	int m;
-
-	if (n != n * m)
-		return (1);
-	else
+	if (n <= 1)
 		return (0);
+	return (prime(n, 2));
 }
 
