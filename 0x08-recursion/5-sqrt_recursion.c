d0fb607af695cfc93eca0bbcb486c263f3f899ce
asdacosta <dacostasilvanus@gmail.com>
2022-02-22 00:46:27 +0000

    Updated: inserted another function that returns sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,42 @@
 #include "main.h"
 
 /**
+ * exponent - Returns natrual square root of number
+ * @n: Input number
+ * @c: Iterator
+ *
+ * Return: Natrual square root or -1 if number has no sqrt
+ */
+int exponent(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + exponent(n, c + 1));
+}
+
+/**
  * _sqrt_recursion - Returns the natural or nonnegative square root
  *	of a number.
  * @n: The number
  *
- * Return: Natural square root, and -1 if n does not have a natrual
+ * Return: Natural square root or -1 if n does not have a natrual
  *	square root.
  */
 int _sqrt_recursion(int n)
 {
-	int m;
-
 	if (n == 1)
 		return (1);
 	else if (n == 0)
 		return (0);
-	else if (n % m == 0)
-		return (_sqrt_recursion(n / m));
+	else if (n < 0)
+		return (-1);
 	else
-		return (-1)
+		return (exponent(n, 2));
 
 }
 
