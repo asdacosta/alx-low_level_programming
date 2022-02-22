cfbcddac0d0d601ff56f130bba054e68e8203eef
asdacosta <dacostasilvanus@gmail.com>
2022-02-22 02:29:45 +0000

    Updated: Inserted two more functions to help check the palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,39 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ *
+ * Return: the length of a string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ *
+ *Return: .
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
  * is_palindrome - Checks if a string reads the same backwards as forwards.
  * @s: Pointer to string
  *
@@ -8,19 +41,8 @@
  */
 int is_palindrome(char *s)
 {
-	int n = 0, m = 0;
-
-	if (n < 0)
-	{
-		if (m >= 0)
-		{
-			if (*(s + n) == *(s + m))
-			{
-				return (1);
-			}
-		}
-	}
-
-	return (0);
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
 
