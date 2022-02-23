d88336b80fc4d5bd8430479125c2b6240ab18a65
asdacosta <dacostasilvanus@gmail.com>
2022-02-23 01:19:39 +0000

    Updated: Multiplies numbers after command line arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,25 @@
-
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - multiplies two numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - Multiplies the two numbers passed on command line after arg
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ *
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int n, mul;
+
+	if (argc > 1)
 	{
-		printf("Error\n");
-		return (1);
+		for (n = 0; n < argc; n++)
+		{
+			mul = argv[1] * argv[2];
+			printf("%d\n", mul);
+		}
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
 	return (0);
 }
+
