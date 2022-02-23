08e7d568d5ef88e719ba634de4598a1883cdfaff
asdacosta <dacostasilvanus@gmail.com>
2022-02-23 01:30:13 +0000

    Deleted: Removed for loop to avoid printing more than once

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,11 +14,8 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 	{
-		for (n = 0; n < argc; n++)
-		{
-			mul = atoi(argv[1]) * atoi(argv[2]);
-			printf("%d\n", mul);
-		}
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
 	}
 	else
 	{
