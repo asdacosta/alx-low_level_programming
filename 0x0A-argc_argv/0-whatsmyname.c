2fabc06b6b12c73d9e520c1a8398d4e26c6d2d07
asdacosta <dacostasilvanus@gmail.com>
2022-02-23 00:02:21 +0000

    Updated: Inserted attribute to allow unused variable argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * my_name - Prints name of the program run on command line
+ * my_name - Prints name of the program or argument run on command line
  * @argc: Number of programs on command line
- * @argv: Program passed on command line
+ * @argv: Array of programs passed on command line
  *
  * Return: Always 0
  */
-int my_name(int argc, char *argv[])
+int my_name(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s", argv[0]);
 
