6d80c22815dcc19a0ebe3f6d54c55b037945d51e
asdacosta <dacostasilvanus@gmail.com>
2022-02-24 02:52:31 +0000

    Updated: Used if statements in macro definition to compute absolute for all numbers

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,14 @@
 #ifndef _FUNCTION_LIKE_MACRO_
 #define _FUNCTION_LIKE_MACRO_
 
-#define ABS(x) (x * -1)
+#include <stdio.h>
 
-#endif
+#define ABS(x) x
+#if x >= 0
+	printf("%d", x);
+#else
+	printf("%d", x * -1);
+#endif /* #define ABS(x) x */
+
+#endif /* #ifndef _FUNCTION_LIKE_MACRO_ */
 
