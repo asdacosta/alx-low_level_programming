3cdb3111bede82ff2468b8a9bd016cf43e6795dc
asdacosta <dacostasilvanus@gmail.com>
2022-02-24 03:03:54 +0000

    Updated: Used ternary condition as token to compute absolute of +/- number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,25 +1,8 @@
 #ifndef _FUNCTION_LIKE_MACRO_
 #define _FUNCTION_LIKE_MACRO_
 
-#include <stdio.h>
 
-#define ABS(x) x
+#define ABS(x) ((x >= 0) ? (x) : (x * -1))
 
-
-/**
- * main - Computes the absolute value of negative or positive number
- *
- * Return: Always 0
- */
-int main(void)
-{
-#if x >= 0
-	printf("%d", x);
-#else
-	printf("%d", x * -1);
-#endif /* #define ABS(x) x */
-
-	return (0);
-}
 #endif /* #ifndef _FUNCTION_LIKE_MACRO_ */
 
