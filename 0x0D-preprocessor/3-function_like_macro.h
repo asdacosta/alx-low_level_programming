e94417c6ff31b07fdbe52a4cebed8af9bd71a81f
asdacosta <dacostasilvanus@gmail.com>
2022-02-24 02:55:45 +0000

    Updated: Declared a function to insert if statements

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,11 +4,22 @@
 #include <stdio.h>
 
 #define ABS(x) x
+
+
+/**
+ * main - Computes the absolute value of negative or positive number
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
 #if x >= 0
 	printf("%d", x);
 #else
 	printf("%d", x * -1);
 #endif /* #define ABS(x) x */
 
+	return (0);
+}
 #endif /* #ifndef _FUNCTION_LIKE_MACRO_ */
 
