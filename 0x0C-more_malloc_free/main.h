25a617f53a3b58ed687dea92a2a694ffc906cf62
asdacosta <dacostasilvanus@gmail.com>
2022-02-24 12:32:15 +0000

    Updated: Attached _realloc prototype to header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,6 +8,7 @@ void *malloc_checked(unsigned int );
 char *string_nconcat(char *, char *, unsigned int );
 void *_calloc(unsigned int, unsigned );
 int *array_range(int, int );
+void *_realloc(void *, unsigned int, unsigned int );
 
 #endif /* #ifndef _MAIN_H_ */
 
